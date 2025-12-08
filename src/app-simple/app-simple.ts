388d38c4a5575532e3815ff255ce800a6249c2bc
Adrian <adrian.kuhn@arcor.de>
2025-12-08 15:26:23 +0100

    getting started(2)

---
+++ src/app-simple/app-simple.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector  : 'app-simple',
+  template  : `<h1>Hello World!</h1>`,
+  styleUrl  : './app-simple.css'
+})
+export class AppSimple {
+  title = 'a-trade-frontend';
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import {AppSimple} from './app-simple/app-simple';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(AppSimple, appConfig)
   .catch((err) => console.error(err));
