a663fd940be0adc993c76ac80d1989def55b4ac4
Adrian <adrian.kuhn@arcor.de>
2025-12-13 02:17:36 +0100

    feat: add Angular Material theming and custom styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,19 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports : [
+    // RouterOutlet,
+    MatSlideToggleModule,
+  ],
+  template: `
+    <h1>{{ title() }}</h1>
+    <div class="main">
+      <mat-slide-toggle>Toggle me!</mat-slide-toggle>
+    </div>
+  `,
+  styleUrl: './app.sass'
 })
 export class App {
   protected readonly title = signal('A-Trade');
