411075765a3c4eb57a66c06a4d1791c87ea517a5
Adrian <adrian.kuhn@arcor.de>
2025-12-13 02:05:52 +0100

    remove unused `app-simple` component and replace it in bootstrap with `App`.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
-import {AppSimple} from './app-simple/app-simple';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {appConfig} from './app/app.config';
+import {App} from './app/app';
 
-bootstrapApplication(AppSimple, appConfig)
+bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
